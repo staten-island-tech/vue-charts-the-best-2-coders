03e58faa4abd538224fbb147d450534d2c37a390
ruthweng <ruthweng333@gmail.com>
2024-03-13 13:17:47 -0400

    hi

---
+++ vuechart/src/views/ChartGraphCards.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        <h2>{{ tree.name }}</h2>
+        <h3>{{ id }}</h3>
+    </div>
+</template>
+
+<script setup>
+const props = ({
+    tree:Object,
+    id: Number
+})
+</script>
+
+<style scoped></style>
