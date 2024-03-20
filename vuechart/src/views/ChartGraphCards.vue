a2c6bf60ee20e7add299862976c93bbacd1d50fa
lbernice <bernicel677@gmail.com>
2024-03-20 12:53:10 -0400

    [

---
+++ vuechart/src/views/ChartGraphCards.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="card">
+        <h2>{{ tree.name }}</h2>
+        <h3>{{ id }}</h3>
+    </div>
+</template>
+
+<script setup>
+const props = 
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vuechart/src/views/HomeView.vue
+++ vuechart/src/views/HomeView.vue
@@ -6,9 +6,17 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-const pokemon = ref('')
+const tree = ref('')
+async function getTree() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/ju3b-rwpy.json')
+  let data = await res.json()
+  tree.value = data.results
+}
+onMounted(() => {
+  getTree()
+})
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
