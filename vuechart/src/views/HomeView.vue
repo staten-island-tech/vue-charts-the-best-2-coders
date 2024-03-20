b5480c5696c74f9c3bbb55db9e1e608484f5a3bc
lbernice <bernicel677@gmail.com>
2024-03-20 12:58:15 -0400

    Squashed commit of the following:
    commit f0625f82b976ba6e9f4709b6444b1257ac0be289
    Author: ruthweng <ruthweng333@gmail.com>
    Date:   Tue Mar 19 13:12:33 2024 -0400
    
        hi
    
    commit 892bebe8361b586fe9bb69b846701df8b34c04d2
    Author: ruthweng <ruthweng333@gmail.com>
    Date:   Mon Mar 18 13:13:36 2024 -0400
    
        okie
    
    commit cd0a43e801e9de07b45f9937b827a043dc5dc81d
    Author: ruthweng <ruthweng333@gmail.com>
    Date:   Thu Mar 14 13:12:36 2024 -0400
    
        okie
    
    commit 03e58faa4abd538224fbb147d450534d2c37a390
    Author: ruthweng <ruthweng333@gmail.com>
    Date:   Wed Mar 13 13:17:47 2024 -0400
    
        hi
    
    commit 8215c287b56b3ffc8b51c7667eecfaf48de1d0f3
    Author: ruthweng <ruthweng333@gmail.com>
    Date:   Mon Mar 11 13:13:29 2024 -0400
    
        hi
    
    commit c1435bd32231b14acf9b01e2571213eefe160b7d
    Author: ruthweng <ruthweng333@gmail.com>
    Date:   Fri Mar 8 13:13:21 2024 -0500
    
        hi
    
    commit a7c1dc88aad0ebc6cc0c4b7f61787cd885b5236a
    Author: ruthweng <ruthweng333@gmail.com>
    Date:   Thu Mar 7 13:12:13 2024 -0500
    
        hi
    
    commit b453c62b92db3a9980922523f477436ff2bb9c00
    Author: ruthweng <ruthweng333@gmail.com>
    Date:   Wed Mar 6 13:12:47 2024 -0500
    
        okk
    
    commit 537a5a5b1245cbf319f70ca4708f4468f7fd84ce
    Author: ruthweng <ruthweng333@gmail.com>
    Date:   Tue Mar 5 13:15:45 2024 -0500
    
        ok

--- vuechart/src/views/HomeView.vue
+++ vuechart/src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <template>
+  <div class="container">
+    <ChartGraphCard v-for="(pee, index + 1) in tree" 
+    :key="pee.name"
+    :id="index + 1" 
+    :tree="pee"/>  
+    </div>
   <div>
 
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import {ref, onMounted} from 'vue'
+import ChartGraphCards from './ChartGraphCards.vue';
 const tree = ref('')
 async function getTree() {
   let res = await fetch('https://data.cityofnewyork.us/resource/ju3b-rwpy.json')
