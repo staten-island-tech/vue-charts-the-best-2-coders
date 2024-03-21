a118c3fe6cd6d4829669b4616f835e0e055b311a
ruthweng <ruthweng333@gmail.com>
2024-03-21 13:12:44 -0400

    mk

---
+++ vuechart/src/components/nycchart.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>{{ Tree.tree }}</h2>
+        <h3>{{ Tree.boroname }}</h3>
+        <h3>{{ Tree.zipcode }}</h3>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps( {
+        Tree: Object
+    })
+</script>
+
+<style scoped>
+
+</style>
