5b4cdd7616f2d80e070a4e4d46ae2461622f2615
ruthweng <ruthweng333@gmail.com>
2024-03-25 13:13:57 -0400

    hooray

--- vuechart/src/views/ChartGraphCards.vue
+++ vuechart/src/views/ChartGraphCards.vue
@@ -1,15 +1,36 @@
 <template>
-    <div class="card">
-        <h2>{{ tree.name }}</h2>
-        <h3>{{ id }}</h3>
+    <div id="card">
     </div>
+
+    <div class="card">
+    <TreeCard v-for="tree in data" :Tree = tree></TreeCard>
+  </div>
 </template>
 
 <script setup>
-const props = ({
-    tree:Object,
-    id: Number
-})
+import nycchartt from '@/components/nycchart.vue';
+const URL = 'https://data.cityofnewyork.us/resource/ju3b-rwpy.json'
+let data = ''
+import { createApp } from 'vue'
+import App from './HomeView.vue';
+
+// function onBeforeMount(callback: () => void): void
+
+async function mounted() {
+  try {
+    let response = await fetch(URL)
+    data = await response.json()
+    console.log(data)
+  } catch (e) {
+    console.error(e)
+  }
+}
+mounted()
+
+createApp(App).mount('#chart')
+
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
