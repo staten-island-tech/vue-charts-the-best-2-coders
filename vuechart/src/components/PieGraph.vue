96dd1f58590d7cf745dc714d9b68f3e94513bfcb
lbernice <bernicel677@gmail.com>
2024-04-01 01:12:31 -0400

    okk

---
+++ vuechart/src/components/PieGraph.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Pie :data="chartData" :options="chartOptions" />
+  </div>
+</template>
+
+<script>
+import { Pie } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+
+ChartJS.register(Tooltip, Legend, ArcElement)
+
+export default {
+  name: 'PieChart',
+  components: { Pie },
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    },
+    chartOptions: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      retrievedData: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
+
+
