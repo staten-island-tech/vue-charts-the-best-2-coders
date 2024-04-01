84ce8b8d026e8185e6f232ebfa78d5ce4eb86533
lbernice <bernicel677@gmail.com>
2024-04-01 12:12:55 -0400

    ok

--- vuechart/src/components/bargraph.vue
+++ vuechart/src/components/bargraph.vue
@@ -1,11 +1,14 @@
  <template>
     <div>
- 
+      <Bar v-if="loaded" :data="chartData" :options="chartOptions" />
     </div>
  </template>
  
  <script setup>
- 
+   import { Bar } from 'vue-chartjs'
+   import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+   ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
  </script>
  
  <style lang="scss" scoped>
--- vuechart/src/views/barchart.vue
+++ vuechart/src/views/barchart.vue
@@ -7,7 +7,7 @@
 <script setup>
 
 </script>
-
+    
 <style lang="scss" scoped>
 
 </style>
