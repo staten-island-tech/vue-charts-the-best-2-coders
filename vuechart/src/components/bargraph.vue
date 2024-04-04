eab74be274b7777d73554b08a583dc7198245892
ruthweng <ruthweng333@gmail.com>
2024-04-04 13:13:21 -0400

    ok

--- vuechart/src/components/bargraph.vue
+++ vuechart/src/components/bargraph.vue
@@ -1,78 +1,33 @@
 <template>
-  <div>
-    <canvas ref="barChart"></canvas>
-  </div>
+  <Bar :data="chartData" :options="chartOptions" />
 </template>
 
 <script>
-import axios from 'axios';
-import Chart from 'chart.js';
+// DataPage.vue
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
+  name: 'BarGraph',
+  components: { Bar },
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    },
+    chartOptions: {
+      type: Object,
+      default: () => {}
+    }
+  },
   data() {
     return {
-      apiData: [],
-      labels: [],
-      counts: [],
-      chart: null
-    };
-  },
-  mounted() {
-    this.fetchData();
-  },
-  methods: {
-    async fetchData() {
-      try {
-        const response = await axios.get('https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=10');
-        this.apiData = response.data;
-
-        // Process data to extract necessary information
-        this.labels = this.apiData.map(item => item.boro_nm);
-        const countsObj = this.apiData.reduce((acc, curr) => {
-          acc[curr.boro_nm] = (acc[curr.boro_nm] || 0) + 1;
-          return acc;
-        }, {});
-        this.counts = this.labels.map(label => countsObj[label]);
-
-        this.renderChart();
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    },
-    renderChart() {
-      const ctx = this.$refs.barChart.getContext('2d');
-      this.chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: this.labels,
-          datasets: [{
-            label: 'Crime Counts',
-            backgroundColor: 'rgba(54, 162, 235, 0.6)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1,
-            data: this.counts,
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          }
-        }
-      });
-    }
-  },
-  watch: {
-    apiData() {
-      this.renderChart();
+      retrievedData: false
     }
   }
-};
-</script>
-
+}
 </script>
 
 <style lang="scss" scoped></style>
